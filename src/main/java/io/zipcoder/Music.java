73db55ff6cdb281fd706292e08e93976ae0d733f
Dejen <dejen@Zipcoders-MacBook-Pro-88.local>
2022-08-06 20:31:58 -0400

    Passed all tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -13,19 +13,23 @@ public class Music {
     public Integer selection(Integer startIndex, String selection) {
         int selUp = Arrays.asList(playList).indexOf(selection);
         int selDown = 0;
+        int SD1 = 0;
+        int SD2 = 0;
         for (int i = 0; i < playList.length; i++){
+            if (playList[i] == selection)
+                SD1 = i;
             for (int j = playList.length -1; j > 0; j--){
-                if (i < j){
-                    selDown = i;
+                if (playList[i] == selection)
+                SD2 = j;
+                if (SD1 < SD2){
+                    selDown = SD1;
                 }else{
-                    selDown = j;
+                    selDown = SD2;
                 }
             }
         }
         int up = selUp - startIndex;
         int down = startIndex + selDown;
-        System.out.println(up);
-        System.out.println(down);
 
         if (up < down) {
             return up;
